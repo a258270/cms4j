e38a1afb5332d19a74ddb72862cd8e6b9c0a5ae2
zmj <a258270@qq.com>
2017-06-17 11:00:14 +0800

    代码生成controller

--- src/main/java/com/crm4j/base/system/createcode/CreatecodeApiController.java
+++ src/main/java/com/crm4j/base/system/createcode/CreatecodeApiController.java
@@ -2,14 +2,17 @@ package com.crm4j.base.system.createcode;
 
 import com.crm4j.base.controller.ApiBaseController;
 import com.crm4j.base.util.LoggerUtil;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by Administrator on 2017/6/17.
  */
 @RestController
+@RequestMapping(value = "/createcode/api")
 public class CreatecodeApiController extends ApiBaseController {
     public CreatecodeApiController() {
         this.logger = LoggerUtil.getLogger(this.getClass());
+        this.menuId = "9";
     }
 }
